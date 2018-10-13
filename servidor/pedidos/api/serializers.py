c185e96d3c070516edef059feefbe9428e07ee3c
piquiferrato <juanferrato1994@gmail.com>
2018-10-13 18:47:10 -0300

    usuario personalizado y nuevo sistema de logueo funcionando

---
+++ servidor/pedidos/api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from . import models
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.CustomUser
+        fields = ('id', 'email', 'username', 'password', 'es_tecnico',)
+
+
+class TokenSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    class Meta:
+        model = Token
+        fields = ('key', 'user',)
