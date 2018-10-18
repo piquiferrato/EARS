5dfc7781f97a03a1ab7c8a345c8d1a679b0eed64
piquiferrato <juanferrato1994@gmail.com>
2018-10-18 12:16:33 -0300

    recreación de usuarios y técnicos

--- servidor/pedidos/api/models.py
+++ servidor/pedidos/api/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     name = models.CharField(blank=True, max_length=255)
-    isTechnician = models.BooleanField(default=True)
+    isTechnician = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
